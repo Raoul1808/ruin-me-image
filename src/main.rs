318ff96526c7188752e985ff2f0676ae36d3b830
Raoul1808 <raoulthegeek@gmail.com>
2025-06-05 15:45:27 +0200

    Change native window size

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,22 @@
 use app::Application;
-use eframe::NativeOptions;
+use eframe::{
+    NativeOptions,
+    egui::{ViewportBuilder, vec2},
+};
 
 mod app;
 
 fn main() -> eframe::Result {
+    let native_options = NativeOptions {
+        viewport: ViewportBuilder {
+            inner_size: Some(vec2(1280., 720.)),
+            ..Default::default()
+        },
+        ..Default::default()
+    };
     eframe::run_native(
         "Ruin me image",
-        NativeOptions::default(),
+        native_options,
         Box::new(|_cc| Ok(Box::new(Application::default()))),
     )
 }
