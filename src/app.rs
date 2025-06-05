7a8da55de11eaf8b85223f3c89cd0e1eb4b3d680
Raoul1808 <raoulthegeek@gmail.com>
2025-06-05 15:05:00 +0200

    Create image viewer app

---
+++ src/app.rs
@@ -0,0 +1,77 @@
+use std::path::PathBuf;
+
+use eframe::{
+    App,
+    egui::{
+        CentralPanel, ColorImage, Image, TextureHandle, TextureOptions, Widget, load::SizedTexture,
+        vec2,
+    },
+};
+use image::{DynamicImage, EncodableLayout};
+
+#[derive(Default)]
+pub struct Application {
+    path: Option<PathBuf>,
+    img_tex: Option<(SizedTexture, TextureHandle)>,
+}
+
+impl App for Application {
+    fn update(&mut self, ctx: &eframe::egui::Context, _frame: &mut eframe::Frame) {
+        CentralPanel::default().show(ctx, |ui| {
+            ui.heading("Hello, image!");
+            ui.horizontal(|ui| {
+                if ui.button("Browse").clicked() {
+                    let file = rfd::FileDialog::new().set_title("Select image").pick_file();
+                    if let Some(file) = file {
+                        match image::open(&file) {
+                            Ok(img) => {
+                                let img = match &img {
+                                    DynamicImage::ImageRgb8(image) => ColorImage::from_rgb(
+                                        [image.width() as usize, image.height() as usize],
+                                        image.as_bytes(),
+                                    ),
+                                    other => {
+                                        let image = other.to_rgba8();
+                                        ColorImage::from_rgba_unmultiplied(
+                                            [image.width() as usize, image.height() as usize],
+                                            image.as_bytes(),
+                                        )
+                                    }
+                                };
+                                let img_size = vec2(img.size[0] as f32, img.size[1] as f32);
+                                let handle =
+                                    ctx.load_texture("preview", img, TextureOptions::default());
+                                let sized_image = SizedTexture::new(handle.id(), img_size);
+                                self.img_tex = Some((sized_image, handle));
+                                self.path = Some(file);
+                            }
+                            Err(e) => {
+                                rfd::MessageDialog::new()
+                                    .set_title("Error loading image")
+                                    .set_level(rfd::MessageLevel::Error)
+                                    .set_description(format!(
+                                        "Error while loading {}: {}",
+                                        file.display(),
+                                        e
+                                    ))
+                                    .show();
+                            }
+                        }
+                    }
+                }
+                ui.label(format!(
+                    "Selected: {}",
+                    self.path
+                        .as_ref()
+                        .map(|d| d.display().to_string())
+                        .unwrap_or("None".into())
+                ))
+            });
+            if let Some((tex, _)) = self.img_tex.as_ref() {
+                ui.separator();
+                ui.label("Image preview");
+                Image::new(*tex).ui(ui);
+            }
+        });
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use app::Application;
+use eframe::NativeOptions;
+
+mod app;
+
+fn main() -> eframe::Result {
+    eframe::run_native(
+        "Ruin me image",
+        NativeOptions::default(),
+        Box::new(|_cc| Ok(Box::new(Application::default()))),
+    )
 }
