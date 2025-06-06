b77037188202acaddd0f76b3f6bb0b167d136899
Raoul1808 <raoulthegeek@gmail.com>
2025-06-06 09:28:14 +0200

    Rename Saturate filter to Brightness

--- src/commands/filter.rs
+++ src/commands/filter.rs
@@ -9,21 +9,21 @@ use image::{
 #[derive(Debug, Clone)]
 pub enum ImageFilter {
     JpegCompression { quality: u8 },
-    Saturate { percentage: u8 },
+    Brightness { percentage: u8 },
 }
 
 impl ImageFilter {
     pub const DEFAULTS: &[ImageFilter] = &[
         Self::JpegCompression { quality: 80 },
-        Self::Saturate { percentage: 100 },
+        Self::Brightness { percentage: 100 },
     ];
 
-    pub const NAMES: &[&str] = &["JPEG Compression", "Saturate"];
+    pub const NAMES: &[&str] = &["JPEG Compression", "Brightness"];
 
     pub fn name(&self) -> &str {
         match self {
             Self::JpegCompression { .. } => Self::NAMES[0],
-            Self::Saturate { .. } => Self::NAMES[1],
+            Self::Brightness { .. } => Self::NAMES[1],
         }
     }
 
@@ -32,9 +32,9 @@ impl ImageFilter {
             Self::JpegCompression { quality } => {
                 Slider::new(quality, 1..=100).text("Quality").ui(ui);
             }
-            Self::Saturate { percentage } => {
+            Self::Brightness { percentage } => {
                 Slider::new(percentage, 0..=200)
-                    .text("Saturation (%)")
+                    .text("Brightness (%)")
                     .ui(ui);
             }
         }
@@ -52,7 +52,7 @@ impl ImageFilter {
                 let decoder = JpegDecoder::new(Cursor::new(bytes)).unwrap();
                 DynamicImage::from_decoder(decoder).unwrap()
             }
-            Self::Saturate { percentage } => {
+            Self::Brightness { percentage } => {
                 let mut img = img;
                 let percent = *percentage as f32 / 100.;
                 for (x, y, col) in img.clone().pixels() {
